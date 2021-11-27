7217adc4fddf8d88d63d1ef3383845eeb6e330f8
JavidelaRubia <javidelarubia@gmail.com>
2021-11-27 12:46:51 +0100

    susmuertos

--- APP/script.js
+++ APP/script.js
@@ -1,61 +1,80 @@
 //Javier de la Rubia Sánchez
-let ctx = document.getElementById("myChart").getContext("2d");
 let labels = ['Ingresos','GastosFijos','GastosVariables'];
 let colorHex = ['#A5DC75','#DCAA75','#BD75DC'];
 let cuenta_DOM;
+
 const cuenta={
     ingreso:10,
     gastoFijo:10,
     gastoVariable:10
 };
+class TipoTrans {
+  static Ingreso = new tipoTrans("Ingreso")
+  static GastosFijos = new tipoTrans("Gastos Fijos")
+  static GastosVariables = new tipoTrans("Gastos Variables")
 
+  constructor(nombre) {
+    this.nombre = nombre
+  }
+}
+class Categoria {
+  static Comida = new tipoTrans("Comida")
+  static Viaje = new tipoTrans("Viaje")
+  static Entretenimiento = new tipoTrans("Entretenimiento")
+
+  constructor(nombre) {
+    this.nombre = nombre
+  }
+}
+class transaccion {
+    static tipo = TipoTrans;
+    static categoria = Categoria;
+    static cantidad = 0;
+    static nombre = "";
+    static fecha = Date;
+}
+
+
+pintaPastel(cuenta.ingreso,cuenta.gastoFijo,cuenta.gastoVariable);
 
 function addIngreso() {
     let cantidad = +document.getElementById("ingreso").value;
     console.log(cantidad);
     cuenta.ingreso= cuenta.ingreso+cantidad;
     console.log(cuenta.ingreso);
+    pintaPastel(cuenta.ingreso,cuenta.gastoFijo,cuenta.gastoVariable);
 }
 
-
-var options = {
-    series: [44, 55, 41, 17, 15],
-    chart: {
-    width: 380,
-    type: 'donut',
-  },
-  plotOptions: {
-    pie: {
-      startAngle: -90,
-      endAngle: 270
-    }
-  },
-  dataLabels: {
-    enabled: false
-  },
-  fill: {
-    type: 'gradient',
-  },
-  legend: {
-    formatter: function(val, opts) {
-      return val + " - " + opts.w.globals.series[opts.seriesIndex]
-    }
-  },
-  title: {
-    text: 'Gradient Donut with custom Start-angle'
-  },
-  responsive: [{
-    breakpoint: 480,
+function pintaPastel(ingreso,gastoFijo,gastoVariable) {
+  document.getElementById("chart-wrapper").innerHTML="";
+  document.getElementById("chart-wrapper").innerHTML="´<canvas id='myChart'></canvas>´";
+  let ctx = document.getElementById("myChart").getContext("2d");
+  let myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      datasets: [{
+        data: [ingreso,gastoFijo,gastoVariable],
+        backgroundColor: colorHex
+      }],
+      labels: labels
+      
+    },
     options: {
-      chart: {
-        width: 200
-      },
+      responsive: true,
       legend: {
-        position: 'bottom'
+        position: 'bottom',
+      
+      },
+      plugins: {
+        datalabels: {
+         
+        },
+        title: {
+          display: true,
+          text: 'Chart Title',
+          
+        }
       }
     }
-  }]
-  };
-
-  var chart = new ApexCharts(document.querySelector("#chart"), options);
-  chart.render();
+  })
+}
